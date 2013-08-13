b66f00dae321f4865b12bc8a29fe8faf958c984b
Tourniquet <latzer.daniel@gmail.com>
2013-08-13 17:15:20 +0200

    Split subcl.rb into Subcl and cli

---
+++ Subcl.rb
@@ -0,0 +1,98 @@
+require_relative 'Mpc'
+require_relative 'Subsonic'
+
+class Subcl
+	attr_reader :player, :subsonic
+
+	def initialize
+		@subsonic = Subsonic.new	
+		@player = Mpc.new
+	end
+
+	def albumartUrl
+		current = @player.current
+		puts @subsonic.albumartUrl(current) unless current.empty?
+	end
+
+	def playSong(name)
+		queueSong(name, true)
+		@player.play
+	end
+
+	def playAlbum(name)
+		queueAlbum(name, true)
+		@player.play
+	end
+
+	def playArtist(name)
+		queueArtist(name, true)
+		@player.play
+	end
+
+	def queueArtist(name, clear = false)
+		songs = @subsonic.artistSongs(name)
+		exit 2 if songs.empty?
+		@player.clear if clear
+
+		songs.each do |song|
+			@player.add(song)
+		end
+	end
+
+	def queueAlbum(name, clear = false)
+		songs = @subsonic.albumSongs(name)
+		exit 2 if songs.empty?
+		@player.clear if clear
+
+		songs.each do |song|
+			@player.add(song)
+		end 
+	end
+
+	def queueSong(name, clear = false)
+		songs = @subsonic.song(name)
+		exit 2 if songs.empty?
+		@player.clear if clear
+
+		songs.each do |song|
+			@player.add(song)
+		end
+	end
+
+	def searchSong(name)
+		songs = @subsonic.songs(name)
+		if(songs.size == 0)
+			$stderr.puts "No matching song"
+			exit 2
+		else
+			songs.each do |song|
+				puts "#{song['title']} by #{song['artist']} on #{song['album']} (#{song['year']})"
+			end
+		end
+	end
+
+	def searchAlbum(name)
+		albums = @subsonic.albums(name)
+		if(albums.size == 0)
+			$stderr.puts "No matching album"
+			exit 2
+		else
+			albums.each do |album|
+				puts "#{album['name']} by #{album['artist']} in #{album['year']}"
+			end
+		end
+	end
+
+	def searchArtist(name)
+		artists = @subsonic.artists(name)
+		if(artists.size == 0)
+			$stderr.puts "No matching artist"
+			exit 2
+		else
+			artists.each do |artist|
+				puts "#{artist['name']}"
+			end
+		end
+	end
+
+end
